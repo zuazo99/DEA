6124cd5143bf1bcaef749464cef19552394a3d06
jonbl <jonbl@10.109.252.9>
2020-09-29 10:05:05 +0200

    El main casi acabado

--- src/webkudeaketa/WebOrriak.java
+++ src/webkudeaketa/WebOrriak.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.Scanner;
 
 public class WebOrriak { //klase hau EMA,singleton patroia
 	//atributuak
@@ -46,6 +47,40 @@ public class WebOrriak { //klase hau EMA,singleton patroia
 		catch(IOException e){
 			e.printStackTrace();
 		}
+	}
+	public static void Nagusia(String[] args) {
+		Scanner eskaner = new Scanner(System.in);
+		System.out.println("Ongi etorri web kudeaketa aplikaziora.");
+		System.out.println("Zer egin nahi duzu?\n");
+		System.out.println("Aukera hauek dituzu:\n");
+		System.out.println("1. Web-orri bat bilatu (1 bat idatzi aukeratzeko)\n ");
+		System.out.println("2. Web-orri berri bat txertatu (2 bat idatzi aukeratzeko)\n ");
+		System.out.println("3. Web-orri bat ezabatu (3 bat idatzi aukeratzeko)\n ");
+		System.out.println("4. Web-orri bat emanda, estekatutako zerrenda bueltatu (4 bat idatzi aukeratzeko)\\n ");
+		System.out.println("5. Gako hitz bat emanda, gako hitza duten web-orrien zerrenda bueltatu (5 bat idatzi aukeratzeko)\n ");
+		System.out.println("6. Web-orrien zerrenda ordenatua lortu (alfabetikoki) (7 bat idatzi aukeratzeko)\n ");
+		Integer aukera=eskaner.nextInt();
+		boolean irten=false;
+		while (!irten) || ((aukera!=1) && (aukera!=2) && (aukera!=3) && (aukera!=4) && (aukera!=5) && (aukera!=6)){
+			//He hecho todo con ifs que seguramente sean 0 eficientes pero creo que es lo mejor.
+			if (aukera == 1){
+				//se ejecutará el método llamado weborriaBilatu(); que buscará una weborri
+			} else if (aukera==2) {
+				//se ejecutará el método llamado weborriaTxertatu(); que meterá una weborri
+			} else if (aukera==3) {
+				//se ejecutará el método llamado weborriaEzabatu(); que borrará una weborri
+			} else if (aukera==4) {
+				//se ejecutará el método llamado getEstekatutakoZerrenda(); (o algo asi) que devolverá una zerrenda de web orri
+			} else if (aukera==5) {
+				//se ejecutará el método llamado getGakoWeborrienZerrenda(); (o algo asi) que devolverá una zerrenda de zerrenda de weborri que contengan el gako hitza introducido
+			} else if (aukera==6) {
+				//se ejecutará el método llamado zerrendaOrdenatuaLortu; que devolverá una lista ordenada de weborri
+			} else {
+				System.out.println("Ez duzu ondo aukeratu, saiatu berriro;");
+				irten=true;
+			}
+		Nagusia(String[]); //Para que se vuelva a ejecutar		
+		
 	}
 	
 }

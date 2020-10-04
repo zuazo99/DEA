ea6cc4f1ba275cfc5258ac8b793b9be205ad29ba
zuazo99 <gzuazo63@gmail.com>
2020-10-04 17:09:39 +0200

    diversos cambios

---
+++ src/Probak/GakoHitzakTest.java
@@ -0,0 +1,24 @@
+package Probak;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GakoHitzakTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetNireGakoHitzak() {
+		fail("Not yet implemented");
+	}
+
+}

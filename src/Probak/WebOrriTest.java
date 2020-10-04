1b8916d3a24f9b21c2a96e11345978744f07ff9b
jonbl <jonbl@DESKTOP-6622KII>
2020-10-04 16:41:15 +0200

    Junits creados

---
+++ src/Probak/WebOrriTest.java
@@ -0,0 +1,54 @@
+package Probak;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebOrriTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testWebOrriStringInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWebOrri() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUrl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUrl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIndizea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIndizea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListaKargatu() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/Probak/WebOrriakTest.java
@@ -0,0 +1,39 @@
+package Probak;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebOrriakTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetNireWebOrriak() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListaKargatu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIrakurriZenb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}

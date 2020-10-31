d9eb7d78182895e24b72ec221d191939d394f9e6
jonblanco <jonblancoupvehu@gmail.com>
2020-10-31 13:14:51 +0100

    ORDERED: add y algunos errores solucionados

--- src/webkudeaketa2/OrderedCircularLinkedList.java
+++ src/webkudeaketa2/OrderedCircularLinkedList.java
@@ -1,11 +1,60 @@
 package webkudeaketa2;
 
-public class OrderedCircularLinkedList<T> extends CircularLinkedList<T> implements OrderedListADT<T>  {
+public class OrderedCircularLinkedList<T extends Comparable<T>> extends CircularLinkedList<T> implements OrderedListADT<T>  {
 
 	public OrderedCircularLinkedList(String info, int count, T elem) {
 		super(info, count, elem);
 		// TODO Auto-generated constructor stub
 	}
-	
 
+
+	@Override
+	public void add(T elem) {
+		Node<T> berria = new Node<T>(elem);
+		boolean lehena =false;
+		boolean irten =false;
+		boolean posbai =false;
+		//hiru boolear, lehena pasatu den jakiteko
+		//hau da, bukle infinitoa ez egiteko, irten, elem jada istan badago, listan ez sartzeko,
+		//eta posbai, elem-ren posizioa aurkitzean aktibatuko dena.
+		if (last.hurrengoa==null){//kasu basikoa, lista hutsa
+			count++;
+			last.hurrengoa=new Node<T>(elem);
+			last.hurrengoa.hurrengoa=last.hurrengoa; //hau da, lehenaren hurrengoa-->lehena
+
+		}else{
+			Node<T> unekoa = last.hurrengoa;
+			while(!posbai && !lehena && !irten){
+				if(unekoa.data.compareTo(elem)>0){//elem ez dago listan eta sartuko dugu
+					count++;
+					if (unekoa==last.hurrengoa){//unekoa lehena bada
+						berria.hurrengoa=last.hurrengoa;
+						last.hurrengoa.hurrengoa=berria;
+						last.hurrengoa=berria;
+					}else{//elem erdian txertatzeko
+						berria.hurrengoa=unekoa;
+						unekoa.hurrengoa=berria.hurrengoa.hurrengoa;
+					}
+					posbai=true;
+				}else{
+					if (unekoa.data.equals(elem)){
+						irten=true;
+					}else {
+						unekoa=unekoa.hurrengoa;
+					}if (unekoa==last.hurrengoa){
+						lehena=true;
+					}
+
+				}if (lehena){//amaieran
+					berria.hurrengoa=unekoa;
+					unekoa.hurrengoa=last.hurrengoa;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void merge(OrderedCircularLinkedList<T> z) {
+
+	}
 }
--- src/webkudeaketa2/OrderedListADT.java
+++ src/webkudeaketa2/OrderedListADT.java
@@ -1,5 +1,6 @@
 package webkudeaketa2;
 
-public interface OrderedListADT<T> extends ListADT<T> {
-
+public interface OrderedListADT<T extends Comparable<T>> extends ListADT<T> {
+    public void add(T elem);
+    public void merge(OrderedCircularLinkedList<T> z);
 }
